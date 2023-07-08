5febcce7482a810f87fb58ef5b39d224c32ffedf
Sanchez Roano Carlos Alonso <22393172@utcancun.edu.mx>
2023-07-08 18:53:27 -0500

    Modificar alertas

--- frontend/src/componentes/CrearUsuario.js
+++ frontend/src/componentes/CrearUsuario.js
@@ -2,10 +2,19 @@ import React, { useState, useEffect } from "react";
 import styles from "../estilos/crear_cuenta.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import sw from "sweetalert";
 
 export default function CrearUsuario() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [eCorreo, setECorreo] = useState("");
+  const [eNombre, setENombre] = useState("");
+  const [eApellido, setEApellido] = useState("");
+  const [eContra, setEContra] = useState("");
+  const [eContra2, setEContra2] = useState("");
+  const [clas1, setClas1] = useState(`${styles.error} ${styles.ocultar}`);
+  const [clas2, setClas2] = useState(`${styles.error} ${styles.ocultar}`);
+  const [clas3, setClas3] = useState(`${styles.error} ${styles.ocultar}`);
+  const [clas4, setClas4] = useState(`${styles.error} ${styles.ocultar}`);
+  const [clas5, setClas5] = useState(`${styles.error} ${styles.ocultar}`);
 
   useEffect(() => {
     const verificarSesion = localStorage.getItem("autenticado");
@@ -28,26 +37,50 @@ export default function CrearUsuario() {
   };
 
   const Enviar = async () => {
-
-    if (!body.Nombre.length || !body.Apellido.length || !body.Correo.length || !body.ConfirmarContrasenia.length) {
-        return sw({ icon: 'warning', title: '¡Oops!', text: 'Debes rellenar todos los campos.' })  
+    setClas1(`${styles.error} ${styles.ocultar}`);
+    setClas2(`${styles.error} ${styles.ocultar}`);
+    setClas3(`${styles.error} ${styles.ocultar}`);
+    setClas4(`${styles.error} ${styles.ocultar}`);
+    setClas5(`${styles.error} ${styles.ocultar}`);
+    if (!body.Nombre.length || !body.Apellido.length || !body.Correo.length || !body.Contrasenia || !body.ConfirmarContrasenia.length) {
+      if (body.Nombre.length===0) { setENombre("Debe llenar todos los campos."); setClas1(styles.error); }
+      if (body.Apellido.length===0) { setEApellido("Debe llenar todos los campos."); setClas2(styles.error); }
+      if (body.Correo.length===0) { setECorreo("Debe llenar todos los campos."); setClas3(styles.error); }
+      if (body.Contrasenia.length===0) { setEContra("Debe llenar todos los campos."); setClas4(styles.error); }
+      if (body.ConfirmarContrasenia.length===0) { setEContra2("Debe llenar todos los campos."); setClas5(styles.error); }
+      return;
     }
 
     const verificarCorreo = await axios.post("http://localhost:8081/verificar", {
-        Correo: body.Correo
+      Correo: body.Correo
     });
 
-    if (verificarCorreo.data.Resultado[0]) return sw({ icon: 'error', title: '¡Oops!', text: 'El correo que ingresaste ya existe.' })
+    if (verificarCorreo.data.Resultado[0]) {
+      setECorreo("El correo que ingresaste ya existe.");
+      setClas3(styles.error);
+      return;
+    }
 
     const correoRegex = /^[\w.-]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+$/;
     const filtrarCaracteres = correoRegex.test(body.Correo);
-    if(!filtrarCaracteres) return sw({ icon: 'error', title: '¡Oops!', text: 'El correo que ingresaste no debe contener caracteres especiales y espacios en blancos.' })
-
+    if (!filtrarCaracteres){
+      setECorreo("El correo que ingresaste no debe contener caracteres especiales y espacios en blancos.");
+      setClas3(styles.error);
+      return;
+    }
     const contraseniaRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
     const comprobarContrasenia = contraseniaRegex.test(body.Contrasenia);
-    if (!comprobarContrasenia) return sw({ icon: 'error', title: '¡Oops!', text: 'La contraseña debe tener mínimo 8 caracteres, mayúsculas y minúsculas, digitos y al menos un caracter especial(?=.*[@$!%*#?&])' })
+    if (!comprobarContrasenia) {
+      setEContra("La contraseña debe tener mínimo 8 caracteres, mayúsculas y minúsculas, digitos y al menos un caracter especial(?=.*[@$!%*#?&])");
+      setClas4(styles.error);
+      return;
+    }
 
-    if (body.Contrasenia !== body.ConfirmarContrasenia) return sw({ icon: 'error', title: '¡Oops!', text: 'Las contraseñas deben coincidir.' }) 
+    if (body.Contrasenia !== body.ConfirmarContrasenia) {
+      setEContra2("Las contraseñas deben coincidir.");
+      setClas5(styles.error);
+      return;
+    }
     try {
       await axios.post(
         "http://localhost:8081/registrarUsuario",
@@ -69,11 +102,24 @@ export default function CrearUsuario() {
       console.log("Error en registrar el usuario: " + error);
     }
   };
-    return (
-        <>
-         <main className={styles.man}>
+  const cambiar = (e) => {
+    const elemento = e.target.id;
+    const input = document.getElementById("contra" + elemento);
+    if (e.target.classList.contains("nf-md-eye")) {
+      e.target.classList.remove("nf-md-eye");
+      e.target.classList.add("nf-md-eye_off");
+      input.type = "text";
+    } else {
+      e.target.classList.remove("nf-md-eye_off");
+      e.target.classList.add("nf-md-eye");
+      input.type = "password";
+    }
+  }
+  return (
+    <>
+      <main className={styles.man}>
         <section className={styles.sec}>
-          <Link to={"/"}>
+          <Link to={"/"} className={styles.img}>
             <img src={require("../images/logo2.png")} alt="" />
           </Link>
           <div>
@@ -81,18 +127,24 @@ export default function CrearUsuario() {
             <p>Apellidos</p>
           </div>
           <div>
-            <input
-              type="text"
-              value={body.Nombre}
-              onChange={cambioEntrada}
-              name="Nombre"
-            />
-            <input
-              type="text"
-              value={body.Apellido}
-              onChange={cambioEntrada}
-              name="Apellido"
-            />
+            <span>
+              <input
+                type="text"
+                value={body.Nombre}
+                onChange={cambioEntrada}
+                name="Nombre"
+              />
+              <aside className={clas1}>{eNombre}</aside>
+            </span>
+            <span>
+              <input
+                type="text"
+                value={body.Apellido}
+                onChange={cambioEntrada}
+                name="Apellido"
+              />
+              <aside className={clas2}>{eApellido}</aside>
+            </span>
           </div>
           <p>Correo electrónico</p>
           <input
@@ -101,22 +153,33 @@ export default function CrearUsuario() {
             onChange={cambioEntrada}
             name="Correo"
           />
+          <aside className={clas3}>{eCorreo}</aside>
           <p>Contraseña</p>
-          <input
-            type="password"
-            placeholder="Debe tener almenos 8 caracteres"
-            value={body.Contrasenia}
-            onChange={cambioEntrada}
-            name="Contrasenia"
-          />
+          <span className={styles.submit}>
+            <input
+              type="password"
+              placeholder="Debe tener almenos 8 caracteres"
+              value={body.Contrasenia}
+              onChange={cambioEntrada}
+              name="Contrasenia"
+              id="contra1"
+            />
+            <i className="nf nf-md-eye" id="1" onClick={cambiar}></i>
+          </span>
+          <aside className={clas4}>{eContra}</aside>
           <p>Confirmar contraseña</p>
-          <input
-            type="password"
-            value={body.ConfirmarContrasenia}
-            onChange={cambioEntrada}
-            name="ConfirmarContrasenia"
-          />
-          <span>
+          <span className={styles.submit}>
+            <input
+              type="password"
+              value={body.ConfirmarContrasenia}
+              onChange={cambioEntrada}
+              id="contra2"
+              name="ConfirmarContrasenia"
+            />
+            <i className="nf nf-md-eye" id="2" onClick={cambiar}></i>
+          </span>
+          <aside className={clas5}>{eContra2}</aside>
+          <span className={styles.submit}>
             <input type="submit" value="Crear cuenta" onClick={Enviar} />
           </span>
           <p className={styles.p}>
@@ -124,6 +187,6 @@ export default function CrearUsuario() {
           </p>
         </section>
       </main>
-        </>
-    );
+    </>
+  );
 }
--- frontend/src/componentes/IniciarSesion.js
+++ frontend/src/componentes/IniciarSesion.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from "react";
 import styles from "../estilos/crear_cuenta.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import sw from "sweetalert";
 
 export default function IniciarSesion() {
-    const navigate = useNavigate();
-    useEffect(() => {
-      const verificarSesion = localStorage.getItem("autenticado");
-      if (verificarSesion) {
-        navigate("/");
-      }
-    }, [navigate]);
+  const navigate = useNavigate();
+  const [eCorreo, setECorreo] = useState("");
+  const [eContra, setEContra] = useState("");
+  const [clas1, setClas1] = useState(`${styles.error} ${styles.ocultar}`);
+  const [clas2, setClas2] = useState(`${styles.error} ${styles.ocultar}`);
+  useEffect(() => {
+    const verificarSesion = localStorage.getItem("autenticado");
+    if (verificarSesion) {
+      navigate("/");
+    } 
+  }, [navigate]);
 
   const [body, setBody] = useState({
     Correo: "",
@@ -24,16 +27,24 @@ export default function IniciarSesion() {
   };
 
   const Enviar = async () => {
+    setClas1(`${styles.error} ${styles.ocultar}`);
+    setClas2(`${styles.error} ${styles.ocultar}`);
     if (!body.Correo.length || !body.Contrasenia) {
-        return sw({ icon: 'warning', title: '¡Oops!', text: 'Debes rellenar todos los campos.' })  
+      if (body.Correo.length===0) { setECorreo("Debe llenar todos los campos."); setClas1(styles.error); }
+      if (body.Contrasenia.length===0) { setEContra("Debe llenar todos los campos."); setClas2(styles.error); }
+      return;
     }
 
     const verificarCorreo = await axios.post("http://localhost:8081/verificar", {
-        Correo: body.Correo
+      Correo: body.Correo
     });
 
     const passcor = verificarCorreo.data.Resultado[0];
-    if (passcor && passcor.Contrasenia !== body.Contrasenia) return sw({ icon: 'error', title: 'Error', text: 'Contraseña incorrecta.' });
+    if (passcor && passcor.Contrasenia !== body.Contrasenia) {
+      setEContra("Contraseña incorrecta."); 
+      setClas2(styles.error);
+      return;
+    }
 
     try {
       const response = await axios.post("http://localhost:8081/login", body);
@@ -43,12 +54,27 @@ export default function IniciarSesion() {
         navigate("/");
         localStorage.setItem("autenticado", true);
       } else {
-        sw({ icon: 'error', title: 'Error', text: 'El usuario que ingresaste no existe...' })  
+        setECorreo("El usuario que ingresaste no existe."); 
+        setClas1(styles.error);  
       }
     } catch (error) {
       console.log("Error en el inicio de sesión: " + error);
     }
   };
+
+  const cambiar = (e) => {
+    const elemento = e.target.id;
+    const input = document.getElementById("contra" + elemento);
+    if (e.target.classList.contains("nf-md-eye")) {
+      e.target.classList.remove("nf-md-eye");
+      e.target.classList.add("nf-md-eye_off");
+      input.type = "text";
+    } else {
+      e.target.classList.remove("nf-md-eye_off");
+      e.target.classList.add("nf-md-eye");
+      input.type = "password";
+    }
+  }
 
   return (
     <main className={styles.man}>
@@ -63,16 +89,22 @@ export default function IniciarSesion() {
           onChange={cambioEntrada}
           name="Correo"
         />
+        <aside className={clas1}>{eCorreo}</aside>
         <p>Contraseña</p>
-        <input
-          type="password"
-          placeholder="Debe tener al menos 8 caracteres"
-          value={body.Contrasenia}
-          onChange={cambioEntrada}
-          name="Contrasenia"
-        />
+        <span className={styles.submit}>
+          <input
+            type="password"
+            placeholder="Debe tener al menos 8 caracteres"
+            value={body.Contrasenia}
+            onChange={cambioEntrada}
+            name="Contrasenia"
+            id="contra1"
+          />
+          <i className="nf nf-md-eye" id="1" onClick={cambiar}></i>
+        </span>
+        <aside className={"error " + clas2}>{eContra}</aside>
         <Link className={styles.olvidar}>¿Olvidaste tu contraseña?</Link>
-        <span>
+        <span className={styles.submit}>
           <input type="submit" value="Iniciar sesión" onClick={Enviar} />
         </span>
         <p className={styles.p}>
