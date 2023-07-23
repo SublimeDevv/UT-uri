d0c877c619339076b3834bbc4abf2958bc9f9059
Sanchez Roano Carlos Alonso <22393172@utcancun.edu.mx>
2023-07-23 01:20:46 -0500

    Detalles - agregando mapa

--- frontend/src/componentes/Detalle.js
+++ frontend/src/componentes/Detalle.js
@@ -38,27 +38,27 @@ export default function Detalle() {
         return (
           <>
             <span className={styles.span}>
-              <figure className={styles.figure}>
-                <img
-                  src={require("../images/" + obtenerImagenes[1])}
-                  alt=""
-                  className={styles.uno}
-                />
-                <img
-                  src={require("../images/" + obtenerImagenes[2])}
-                  alt=""
-                  className={styles.dos}
-                />
-                <img
-                  src={require("../images/" + obtenerImagenes[3])}
-                  alt=""
-                  className={styles.tres}
-                />
-              </figure>
+              <article id="borrar" className={styles.borroso}>
+                
+              </article>
+              <aside>
+                <figure className={styles.figure}>
+                  <img
+                    src={require("../images/" + obtenerImagenes[1])}
+                    alt=""
+                    className={styles.uno}
+                  />
+                </figure>
+                <div className={styles.contenedor}>
+                  <span className={styles.mapa}>
+
+                  </span>
+                </div>
+              </aside>
+              
               <div key={index} className={styles.div}>
                 <h1>{detalle.Nombre}</h1>
                 <p className={styles.p}>{detalle.Descripcion}</p>
-                <p className={styles.p}></p>
                 <span>
                   <figure className={styles.fig}>
                     <div className={styles.orden}>
