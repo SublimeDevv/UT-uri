10045732d4eb48bfd31f56ce4c6d26f68383e5d6
SublimeDevv <juanmen1404@gmail.com>
2023-07-23 17:39:16 -0500

    Agregar endpoint subcategorias #1

--- backend/Routes/subcategorias.js
+++ backend/Routes/subcategorias.js
@@ -3,6 +3,19 @@ import conexion from "../dbconfig.js";
 
 const router = new Router();
 
+router.get("/ObtenerSubcategorias/:lugarId", (peticion, respuesta) => {
+  const lugarId = peticion.params.lugarId;
+  const query = "CALL SP_ObtenerSubcategorias(?)";
+  conexion.query(query, [lugarId], (error, resultados) => {
+    if (error) {
+      console.error("Error al obtener las subcategorías: ", error);
+      respuesta.status(500).json({ Error: "Error al obtener las subcategorías del lugar" });
+    } else {
+      respuesta.json({ Estatus: "EXITOSO", Resultado: resultados[0] });
+    }
+  });
+});
+
 router.post("/AgregarSubcategoriaLugar", (peticion, respuesta) => {
     const { lugarId, subcategoriaId } = peticion.body;
     const query = "CALL SP_Agregar_Lugar_Subcategoria(?, ?)";
